3faad778f756072f72e38920b6a181445b687a61
Nicolas signed-log FORMICHELLA <stigpro@outlook.fr>
2024-04-29 23:18:54 +0200

    Remove unnecessary, setup DARK_THEME recognition

--- .config/zsh/zenv.zsh
+++ .config/zsh/zenv.zsh
@@ -3,26 +3,21 @@ export PATH=$HOME/.emacs.d/bin:$HOME/.cargo/bin:$SPICETIFY_INSTALL:$HOME/.local/
 export MANPATH=/usr/local/man:$MANPATH
 export GDB_HISTFILE="$HOME/.gdb_history"
 export ZSH_CACHE_DIR="$HOME/.local/share"
-# export DOOMDIR="${XDG_CONFIG_HOME:-${HOME}/.config}/doom"
 
-export VISUAL=nvim
-export EDITOR=nvim
+if type nvim &> /dev/null; then
+  export VISUAL=nvim
+  export EDITOR=nvim
+elif type vim &> /dev/null; then
+  export VISUAL=vim
+  export EDITOR=vim
+fi
 
 export DISABLE_AUTO_TITLE='true'
 export MONITOR=all
 
-if command -v batman >/dev/null 2>&1 && command -v bat >/dev/null 2>&1; then
-  #export MANPAGER="batman"
-fi
-
 # Conditionals
 
 OSVER=$( (lsb_release -ds || cat /etc/*release || uname -om) 2>/dev/null | head -n1)
-
-if command -v most >/dev/null 2>&1; then
-  export PAGER=most
-  export DELTA_PAGER=most
-fi
 
 if [ -d "/opt/cuda/bin" ]; then
   export PATH="/opt/cuda/bin:$PATH"
@@ -32,6 +27,14 @@ if [ -d "$HOME/dotnet" ]; then
   export PATH=$PATH:$HOME/dotnet
   export DOTNET_ROOT=$HOME/dotnet
   export DOTNET_CLI_TELEMETRY_OPTOUT=1
+fi
+
+if command -v go >/dev/null 2>&1; then
+  export PATH="$(go env GOPATH)/bin:$PATH"
+fi
+
+if type rvm &> /dev/null; then
+  export PATH="$HOME/.rvm/bin:$PATH"
 fi
 
 #Plugin config
@@ -57,3 +60,20 @@ fi
 
 export GPG_TTY=$(tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
+
+export BAT_THEME="Catppuccin Latte"
+export DARK_THEME=1
+
+if [[ $DESKTOP_SESSION == "gnome" ]] && type ; then
+  if [[ $(gsettings get org.gnome.desktop.interface color-scheme) =~ 'dark' ]]; then
+    export BAT_THEME="Catppuccin Mocha"
+    export DARK_THEME=1
+  else
+    export BAT_THEME="Catppuccin Latte"
+    export DARK_THEME=0
+  fi
+else
+  # Fallback if not GNOME
+  export BAT_THEME="Catppuccin Latte"
+  export DARK_THEME=1
+fi
